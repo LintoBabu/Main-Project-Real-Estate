3114578d504fcb372ea06e35a3584b294c397c3f
Linto Babu <lintobabu4u121@gmail.com>
2025-05-31 14:40:40 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user_route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then (()=>{
@@ -9,8 +10,10 @@ mongoose.connect(process.env.MONGO).then (()=>{
     console.log(err)
 })
 
-const App = express()
+const app = express()
 
-App.listen(3000,()=>{
+app.listen(3000,()=>{
     console.log("sever is running on port 3000!")
 })
+
+app.use("/api/user", userRouter);
