f6ac38c0c41521c4d39408413f04b4e2fbdc09f8
Linto Babu <lintobabu4u121@gmail.com>
2025-05-31 15:34:45 +0530

    created Middleware and a function to handle possible erros

--- api/index.js
+++ api/index.js
@@ -1,23 +1,36 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user_route.js';
-import authRouter from './routes/auth_route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user_route.js";
+import authRouter from "./routes/auth_route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then (()=>{
-    console.log('Connected to DB')
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log("sever is running on port 3000!")
-})
+app.listen(3000, () => {
+  console.log("sever is running on port 3000!");
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Sever Error";
+  return res.status(statusCode).json({
+    sucess: false,
+    statusCode,
+    message,
+  });
+});
