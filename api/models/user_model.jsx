e360b144bd99580514a356f336a8da7bc29c8709
Linto Babu <lintobabu4u121@gmail.com>
2025-05-31 14:15:29 +0530

    create user model

---
+++ api/models/user_model.jsx
@@ -0,0 +1,25 @@
+import { timestamps } from 'bson';
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.userSchema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
