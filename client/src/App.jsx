7f8ac5c1b898167f222f81af578c109db73bd863
Linto Babu <lintobabu4u121@gmail.com>
2025-06-01 00:09:43 +0530

    completed signUp page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Logout from './pages/Logout';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
     <Routes>
      <Route path='/' element={<Home/>}></Route>
      <Route path='/login' element={<Login/>}></Route>
-     <Route path='/logout' element={<Logout/>}></Route>
+     <Route path='/signup' element={<SignUp/>}></Route>
      <Route path='/about' element={<About/>}></Route>
      <Route path='/profile' element={<Profile/>}></Route>
     </Routes>
