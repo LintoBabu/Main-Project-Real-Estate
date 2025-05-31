47033d40e29ed9abbe288ee027352eba76c87d47
Linto Babu <lintobabu4u121@gmail.com>
2025-05-31 13:01:37 +0530

    created header components

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Login from './pages/Login';
 import Logout from './pages/Logout';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
 
     <BrowserRouter>
+    <Header/>
 
     <Routes>
      <Route path='/' element={<Home/>}></Route>
