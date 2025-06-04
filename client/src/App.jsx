5acc9ea0573c54589bca40269a007b21992812d8
Linto Babu <lintobabu4u121@gmail.com>
2025-06-04 14:04:48 +0530

    Completed Profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,24 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
 
     <BrowserRouter>
-    <Header/>
+      <Header />
 
-    <Routes>
-     <Route path='/' element={<Home/>}></Route>
-     <Route path='/signin' element={<SignIn/>}></Route>
-     <Route path='/signup' element={<SignUp/>}></Route>
-     <Route path='/about' element={<About/>}></Route>
-     <Route path='/profile' element={<Profile/>}></Route>
-    </Routes>
-    
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/signin' element={<SignIn />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
+      </Routes>
+
     </BrowserRouter>
   )
 }
